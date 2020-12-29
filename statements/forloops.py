c0b809f5969837c4754dbe862ef7f9acd75686da
David Colatti <davidcolatti@gmail.com>
2020-12-29 16:46:39 -0500

    feat: for loops

---
+++ statements/forloops.py
@@ -0,0 +1,63 @@
+# For Loops
+
+# mylist = [1,2,3,4,5,6,7,8,9,10]
+# for num in mylist:
+#     print(num)
+#     print("Whatever I want")
+
+
+# for num in mylist:
+#     # Check for even numbers
+#     if(num % 2 == 0):
+#         print(num)
+#     else:
+#         print(f"Odd Number: {num}")
+
+
+# list_sum = 0
+# for num in mylist:
+#     list_sum = list_sum + num
+
+# print(list_sum) # 55
+
+# mystring = "Hello World"
+# for letter in mystring:
+#     print(letter)
+
+
+# for _ in "Hello World":
+#     print("Cool!") # Prints Cool! the length of hello world
+
+# tup = (1,2,3)
+# for item in tup:
+#     print(item)
+
+# mylist = [(1,2), (3,4), (5,6), (7,8)]
+# # print(len(mylist)) # 4
+# for a,b in mylist: # Called tuple unpacking
+#     print(a)
+#     print(b)
+
+
+# mylist = [(1,2,3), (4,5,6), (7,8,9)]
+# for a,b,c in mylist:
+#     print(a)
+#     print(b)
+#     print(c)
+
+
+# d = {'k1': 1, 'k2': 2, 'k3': 3}
+# for item in d:
+#     print(item) # prints only keys in a dictionary
+
+# for item in d.items():
+#     print(item) # prints key and values in tuple: ('k1', 1)
+
+# for key, value in d.items():
+#     print(value) # prints value when doing tuple unpacking
+
+# for keys in d.keys():
+#     print(keys) # prints keys
+
+# for value in d.values():
+#     print(value) # prints value
---
+++ statements/main.py
@@ -0,0 +1,28 @@
+# if elif else statements
+
+# hungry = False
+
+# if hungry:
+#     print('Feed Me!')
+# else:
+#     print('Im not hungry')
+
+
+# loc = "Bank"
+
+# if loc == "Auto Shop":
+#     print("Cars are nice")
+# elif loc == "Bank":
+#     print("Money is nice")
+# else:
+#     print("I dont know much")
+
+
+# name = "Sammy"
+
+# if name == "Frankie":
+#     print("Hey Frankie")
+# elif name == "Sammy":
+#     print("Hey Sammy")
+# else:
+#     print("Whats your name?")
